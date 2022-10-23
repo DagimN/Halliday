943933205816812c2f6ac6ab29f519e6e8b4c463
DagimN <dagmnebiat@gmail.com>
2022-10-23 21:30:47 +0300

    Noise clearing algorthim implemented(can be improved), 3D construction partially implemented(prototype working)

--- 3DConstructor.py
+++ 3DConstructor.py
@@ -1,141 +1,90 @@
-#NOTE: Shadow angle(Black surface) can include height information to get from
+#NOTE: Shadow angle(Black surface) can include height information
 import cv2
 import PIL
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from stl import mesh
+
 plt.style.use('seaborn')
-
-maskLocation = 'dataset\\Building\\val_images\\SMTile10008.png'
+maskLocation = 'dataset\\Building\\val_images\\SMTile10003.png'
 tempFilePath = 'tempRaw.png'
-tempCleanFilePath = 'tempClean.png'
 tempVerticeFilePath = 'tempVertices.png'
-NOISE_THRESHOLD = 80
+tempCleanFilePath = 'tempClean.png'
+NOISE_THRESHOLD = 35
 
-def searchNoise(viewedPixelCoordinates, pendingCoordinates, center, mapObject, directions, edged_image):
-    pathExists = False
-
-    if not viewedPixelCoordinates.__contains__(center):
-        viewedPixelCoordinates.append(center)
-
-        if edged_image[center[1], center[0]] == 255:
-            mapObject.append(center)
-
-            for direction in directions:
-                if edged_image[direction[1], direction[0]] == 255 and not mapObject.__contains__(direction):
-                    pendingCoordinates.append(direction)
-                    mapObject.append(direction)
-                    pathExists = True
-                    travelPath(direction[0], direction[1], viewedPixelCoordinates, pendingCoordinates, edged_image, mapObject, 1)    
-                
-        if not pathExists:
-            if len(pendingCoordinates) == 0:
-                clearPixels(edged_image, mapObject)
-            else:
-                coords = pendingCoordinates[0]
-                pendingCoordinates.remove(pendingCoordinates[0])
-                travelPath(coords[0], coords[1], viewedPixelCoordinates, pendingCoordinates, edged_image, mapObject, 1)    
-
-def searchVertices(validPixelCoordinates, directions, AREA_VIEW, edged_image, center):
-    rows = edged_image.shape[0]
-    columns = edged_image.shape[1]
-    if not validPixelCoordinates.__contains__(center):
-            
-        for direction in directions:
-            for i in range(AREA_VIEW):
-                if direction == directions[0] or direction == directions[2]:
-                    posPointCoord = (direction[1], direction[0] + i if direction[0] + i < rows else direction[0])
-                    negPointCoord = (direction[1], direction[0] - i if direction[0] - i < rows else direction[0])
-                        
-                    if edged_image[posPointCoord] == 255 and not validPixelCoordinates.__contains__(posPointCoord):
-                        edged_image[posPointCoord] = 128
-                        validPixelCoordinates.append(posPointCoord)
-                        
-                    if edged_image[negPointCoord] == 255 and not validPixelCoordinates.__contains__(negPointCoord):
-                        edged_image[negPointCoord] = 128
-                        validPixelCoordinates.append(negPointCoord)
-                else:
-                    posPointCoord = (direction[1] + i if direction[1] + i < columns else direction[1], direction[0])
-                    negPointCoord = (direction[1] - i if direction[1] - i < columns else direction[1], direction[0])
-
-                    if edged_image[posPointCoord] == 255 and not validPixelCoordinates.__contains__(posPointCoord):
-                        edged_image[posPointCoord] = 128
-                        validPixelCoordinates.append(posPointCoord)
-                        
-                    if edged_image[negPointCoord] == 255 and not validPixelCoordinates.__contains__(negPointCoord):
-                        edged_image[negPointCoord] = 128
-                        validPixelCoordinates.append(negPointCoord)
-    
-def chooseVertices(clear_edged_image):
-    vertices_image = clear_edged_image
-    verticeCoordinates = []
-    AREA_VIEW = 7
-
-    print('Choosing Vertices =>')
-
-    for x in range(0, clear_edged_image.shape[0], AREA_VIEW):
-        for y in range(0, clear_edged_image.shape[1], AREA_VIEW):
-            travelPath(x, y, verticeCoordinates, None, vertices_image, None, AREA_VIEW)
-
-    vertices_image = np.array(vertices_image, dtype=np.dtype('f8'))
-    cv2.imwrite(tempVerticeFilePath, vertices_image)
-    return cv2.imread(tempVerticeFilePath)
-
-def travelPath(xIndex, yIndex, viewedPixelCoordinates, pendingCoordinates, edged_image, mapObject, AREA_VIEW):
-    rows = edged_image.shape[0]
-    columns = edged_image.shape[1]
-
-    north = (xIndex - AREA_VIEW if xIndex - AREA_VIEW >= 0 else 0, yIndex) 
-    northEast = (xIndex - AREA_VIEW if xIndex - AREA_VIEW >= 0 else 0, yIndex + AREA_VIEW if yIndex + AREA_VIEW < columns else columns - 1)     
-    northWest = (xIndex - AREA_VIEW if xIndex - AREA_VIEW >= 0 else 0, yIndex - AREA_VIEW if yIndex - AREA_VIEW >= 0 else 0)  
-    south = (xIndex + AREA_VIEW if xIndex + AREA_VIEW < rows else rows - 1, yIndex) 
-    southEast = (xIndex + AREA_VIEW if xIndex + AREA_VIEW < rows else rows - 1, yIndex + AREA_VIEW if yIndex + AREA_VIEW < columns else columns - 1)  
-    southWest = (xIndex + AREA_VIEW if xIndex + AREA_VIEW < rows else rows - 1, yIndex - AREA_VIEW if yIndex - AREA_VIEW >= 0 else 0)  
-    east = (xIndex, yIndex + AREA_VIEW if yIndex + AREA_VIEW < columns else columns - 1) 
-    west = (xIndex, yIndex - AREA_VIEW if yIndex - AREA_VIEW >= 0 else 0) 
-    center = (xIndex, yIndex)
-
-    if AREA_VIEW == 1:
-        directions = [north, northEast, east, southEast, south, southWest, west, northWest]
-        searchNoise(viewedPixelCoordinates, pendingCoordinates, center, mapObject, directions, edged_image)
-    else:
-        directions = [north, east, south, west]
-        searchVertices(viewedPixelCoordinates, directions, AREA_VIEW, edged_image, center)
-                           
-def clearPixels(edged_image, mapObject):
-    size = len(mapObject)
-    if not size == 0:
-        pixelColor =  255 if size % 255 > NOISE_THRESHOLD else 0 
-
-        for pixel in mapObject:
-            edged_image[pixel[1], pixel[0]] = pixelColor
-                                        
-        mapObject.clear() 
-                                    
 def cleanNoise(edged_image):
-    rows = edged_image.shape[0] 
+    rows = edged_image.shape[0]
     columns = edged_image.shape[1]
-    yIndex = 0
-    xIndex = 0
-    viewedPixelCoordinates = []     
-    mapObject = []
-    validCoordinates = []    
-    pendingCoordinates = []
+    viewedPixelCoordinates = []
+    validPixelCoordinates = []
+    mapObjects = []
 
-    print('Cleaning Noise =>')
+    for x in range(rows):
+        for y in range(columns):
+            travelPath(x, y, viewedPixelCoordinates, validPixelCoordinates, mapObjects, edged_image)
 
-    while yIndex < columns:
-        xIndex = 0
-        while xIndex < rows:
-            travelPath(xIndex, yIndex, viewedPixelCoordinates, pendingCoordinates, edged_image, mapObject, 1)
-            xIndex += 1
-                    
-        yIndex += 1
-
+    for mapObject in mapObjects:
+        for pixel in mapObject:
+            if len(mapObject) < NOISE_THRESHOLD:
+                edged_image[pixel[1], pixel[0]] = 0
+    
     edged_image = np.array(edged_image, dtype=np.dtype('f8'))
     cv2.imwrite(tempCleanFilePath, edged_image)
     return cv2.imread(tempCleanFilePath)   
 
+def chooseVertices(edged_image):
+    vertices_image = edged_image
+    verticeCoordinates = []
+    AREA_VIEW = 3
+
+    print('Choosing Vertices =>')
+    
+    #TODO: Sort map objects and remove pixels per interval
+    vertices_image = np.array(vertices_image, dtype=np.dtype('f8'))
+    cv2.imwrite(tempVerticeFilePath, vertices_image)
+    return {'coords': np.array(verticeCoordinates), 'image': cv2.imread(tempVerticeFilePath)}
+
+def travelPath(xIndex, yIndex, viewedPixelCoordinates, validPixelCoordinates, mapObjects, edged_image):
+    rows = edged_image.shape[0]
+    columns = edged_image.shape[1]
+
+    north = (xIndex - 1 if xIndex - 1 >= 0 else 0, yIndex) 
+    northEast = (xIndex - 1 if xIndex - 1 >= 0 else 0, yIndex + 1 if yIndex + 1 < columns else columns - 1)     
+    northWest = (xIndex - 1 if xIndex - 1 >= 0 else 0, yIndex - 1 if yIndex - 1 >= 0 else 0)  
+    south = (xIndex + 1 if xIndex + 1 < rows else rows - 1, yIndex) 
+    southEast = (xIndex + 1 if xIndex + 1 < rows else rows - 1, yIndex + 1 if yIndex + 1 < columns else columns - 1)  
+    southWest = (xIndex + 1 if xIndex + 1 < rows else rows - 1, yIndex - 1 if yIndex - 1 >= 0 else 0)  
+    east = (xIndex, yIndex + 1 if yIndex + 1 < columns else columns - 1) 
+    west = (xIndex, yIndex - 1 if yIndex - 1 >= 0 else 0) 
+    center = (xIndex, yIndex)
+
+    directions = [north, northEast, east, southEast, south, southWest, west, northWest]
+    pathExists = False
+    
+    if not mapObjects == None:
+        if not viewedPixelCoordinates.__contains__(center):
+            viewedPixelCoordinates.append(center)
+            
+            if edged_image[center[1], center[0]] == 255:
+                validPixelCoordinates.append(center)
+                
+                for direction in directions: 
+                    if edged_image[direction[1], direction[0]] == 255:
+                        pathExists = True
+                        travelPath(direction[0], direction[1], viewedPixelCoordinates, validPixelCoordinates, mapObjects, edged_image) 
+
+            if not pathExists and not len(validPixelCoordinates) == 0:
+                mapObject = []
+                
+                for pixel in validPixelCoordinates:
+                    mapObject.append(pixel)
+                
+                mapObjects.append(mapObject)
+                validPixelCoordinates.clear()
+    else:
+        searchVertices(viewedPixelCoordinates, directions, edged_image, center) 
+                          
 def showImages(loaded_image, edged_image, clear_edged_image, vertices_image):
     plt.figure(figsize=(50,50))
     
@@ -161,6 +110,31 @@ def showImages(loaded_image, edged_image, clear_edged_image, vertices_image):
 
     plt.show()
 
+def createMesh(verticesCoords):
+    vertices = []
+    faces = []
+    for coords in verticesCoords:
+        vertices.append((coords[0], coords[1], 0))
+    for coords in verticesCoords:
+        vertices.append((coords[0], coords[1], 30))
+
+    for index, vertice in enumerate(vertices):
+        if vertice[2] == 0:
+            faces.append([index, (index + 1) % len(vertices), vertices.index((vertice[0], vertice[1], 30))])
+        else:
+            faces.append([index, (index + 1) % len(vertices), vertices.index((vertice[0], vertice[1], 0))])
+
+    vertices = np.array(vertices)
+    faces = np.array(faces)
+    cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+    
+    for i, face in enumerate(faces):
+        for j in range(3):
+            cube.vectors[i][j] = vertices[face[j], :]
+
+    cube.save('cube.stl')
+    print("DONE!!!")
+
 def main():
     print('Started => ')
     loaded_image = cv2.imread(maskLocation)
@@ -172,15 +146,15 @@ def main():
     cv2.imwrite(tempFilePath, non_black_image)
     non_black_image = cv2.imread(tempFilePath)
 
-    edged_image = cv2.Canny(non_black_image, threshold1=30, threshold2=260)
+    edged_image = cv2.Canny(non_black_image, threshold1=64, threshold2=512)
     cv2.imwrite(tempFilePath, edged_image)
     
-    clear_edged_image = cleanNoise(edged_image=edged_image)
-    vertices_image = cv2.imread(tempCleanFilePath)
-    vertices_image = cv2.cvtColor(vertices_image,cv2.COLOR_BGR2GRAY)
-    vertices_image = chooseVertices(vertices_image)
+    clear_edged_image = cleanNoise(edged_image)
+    vertices_image = cv2.cvtColor(clear_edged_image,cv2.COLOR_BGR2GRAY)
+    verticesDict= chooseVertices(vertices_image)
 
-    showImages(loaded_image=loaded_image, edged_image=cv2.imread(tempFilePath), clear_edged_image=clear_edged_image, vertices_image=vertices_image)
+    createMesh(verticesDict['coords'])
+    showImages(loaded_image=loaded_image, edged_image=cv2.imread(tempFilePath), clear_edged_image=clear_edged_image, vertices_image=verticesDict['image'])
 
 if __name__ == "__main__":
     main()
--- Halliday.py
+++ Halliday.py
@@ -1,32 +1,184 @@
-import numpy as np
-import pandas
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms.functional as TF
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_predictions_as_imgs,
+)
 
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
-from keras.preprocessing.image import ImageDataGenerator   
-from keras import utils as image
+increment = 1
+LEARNING_RATE = 1e-4
+DEVICE = "cpu"
+BATCH_SIZE = 1
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 150
+IMAGE_WIDTH = 150
+PIN_MEMORY = True
+LOAD_MODEL = True
+TRAIN_IMG_DIR = "dataset/training/Building/Batches/batch_1/train_images"
+TRAIN_MASK_DIR = "dataset/training/Building/Batches/batch_1/train_masks"
+VAL_IMG_DIR = "dataset/Building/val_images"
+VAL_MASK_DIR = "dataset/Building/val_masks" 
 
-Halliday = Sequential()
-Halliday.add(Conv2D(32, (3,3), input_shape=(150, 150, 3), activation='relu'))
-Halliday.add(MaxPooling2D(pool_size=(2,2)))
-Halliday.add(Flatten())
-Halliday.add(Dense(units=128, activation='relu'))
-Halliday.add(Dense(units=1, activation='sigmoid'))
-Halliday.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
 
-train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
-test_datagen = ImageDataGenerator(rescale=1./255)
+            nn.Conv2d(out_channels, out_channels, 3, 1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
 
-training_set = train_datagen.flow_from_directory("dataset/training/", target_size=(150,150), batch_size=2, class_mode='binary')
-test_set = test_datagen.flow_from_directory("dataset/test/", target_size=(150,150), batch_size=2, class_mode='binary')
+    def forward(self, x):
+        return self.conv(x)
 
-Halliday.fit(training_set, steps_per_epoch=len(training_set), epochs=10, validation_data=test_set, validation_steps=len(test_set))
+class UNET(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
+        super(UNET,self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-test_image = image.load_img('dataset/predict/roof1.png')
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis=0)
-result = Halliday.predict(test_image)
-print(result)
+        for feature in features:
+                self.downs.append(DoubleConv(in_channels, feature))
+                in_channels = feature
+
+        for feature in reversed(features):
+            self.ups.append(nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2))
+            self.ups.append(DoubleConv(feature*2, feature))
+        
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+        
+        if x.shape[3] == 3:
+            x = x.permute(0, 3, 1, 2)
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
+
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
+                
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx+1](concat_skip)
+        
+        
+        return self.final_conv(x)
+        
+def test():
+    x = torch.randn((3, 1, 160, 160))
+    model = UNET(in_channels=1, out_channels=1)
+    preds = model(x)
+    
+    assert preds.shape == x.shape
+
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+        loop = tqdm(loader)
+
+        for batch_idx, (data, targets) in enumerate(loop):
+            data = data.to(device=DEVICE)
+            targets = targets.float().unsqueeze(1).to(device=DEVICE)
+
+            # forward
+            with torch.cpu.amp.autocast():
+                predictions = model(data)
+                loss = loss_fn(predictions, targets)
+            
+            # backward
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            loop.set_postfix(loss=loss.item())
+
+def main():
+    global increment
+    train_transforms = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2()
+        ]
+    )
+
+    val_transforms = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0,1.0],
+                max_pixel_value=255.0
+            )
+        ]
+    )
+
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transforms,
+        val_transforms,
+        NUM_WORKERS,
+        PIN_MEMORY
+    )
+
+    if LOAD_MODEL:
+        load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
+
+    check_accuracy(val_loader, model, device=DEVICE)
+    scaler = torch.cuda.amp.GradScaler()
+
+    for epoch in range(NUM_EPOCHS):
+        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict()
+        }
+        save_checkpoint(checkpoint)
+
+        check_accuracy(val_loader, model, device=DEVICE)
+
+        save_predictions_as_imgs(val_loader, model, folder="saved_images/", device=DEVICE)
+
+if __name__ == "__main__":
+    main()
